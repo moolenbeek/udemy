935e2940d5a261eb2eec529875785c7ab9cd2e6c
Eric - Wall Sound <emoolenbeek@gmail.com>
2024-07-16 08:17:19 -0400

    add typecript boolean

--- typescript/types/app.ts
+++ typescript/types/app.ts
@@ -2,18 +2,23 @@
 
 // number type
 
-function add (number1: number, number2: number) {
-    console.log(`type: ${typeof number1}`);
+function add(number1: number, number2: number, showResult: boolean) {
+  console.log(`type: ${typeof number1}`);
 
-    // this js method gets type at runtime
-    // if (typeof number1 !== 'number' || typeof number2 !== 'number') {
-    //     throw new Error('Incorrect input!')
-    // }
-    return +number1 + +number2;
+  // this js method gets type at runtime
+  // if (typeof number1 !== 'number' || typeof number2 !== 'number') {
+  //     throw new Error('Incorrect input!')
+  // }
+
+  if (showResult) {
+    console.log(number1 + number2);
+  }
+  return number1 + number2;
 }
 
 const number1 = 10;
 const number2 = 5;
+const printResult = true;
 
-const result = add(number1, number2);
+const result = add(number1, number2, printResult);
 console.log(`result: ${result}`);
