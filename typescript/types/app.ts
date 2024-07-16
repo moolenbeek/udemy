4fded4fedc07665f4f1880c65b387e33a086a280
Eric - Wall Sound <emoolenbeek@gmail.com>
2024-07-16 08:23:38 -0400

    add typescript string

--- typescript/types/app.ts
+++ typescript/types/app.ts
@@ -2,22 +2,23 @@
 
 // number type
 
-function add(number1: number, number2: number, showResult: boolean) {
-  console.log(`type: ${typeof number1}`);
+function add(number1: number, number2: number, showResult: boolean, phrase: string) {
 
   // this js method gets type at runtime
   // if (typeof number1 !== 'number' || typeof number2 !== 'number') {
   //     throw new Error('Incorrect input!')
   // }
 
+  const result = number1 + number2;
   if (showResult) {
-    console.log(number1 + number2);
+    console.log(phrase + result);
   }
-  return number1 + number2;
+  return result;
 }
 
 const number1 = 10;
 const number2 = 5;
 const printResult = true;
+const resultPhrase = 'Result is: ' 
 
-add(number1, number2, printResult);
+add(number1, number2, printResult, resultPhrase);
