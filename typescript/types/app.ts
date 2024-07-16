02cef5eb9c42ec3c1608fd2ad811ad9d0d70341e
Eric - Wall Sound <emoolenbeek@gmail.com>
2024-07-15 23:48:19 -0400

    add number type

--- typescript/types/app.ts
+++ typescript/types/app.ts
@@ -1,1 +1,13 @@
 // types
+
+// number type
+
+function add (number1: number, number2: number) {
+    return +number1 + +number2;
+}
+
+const number1 = 10;
+const number2 = 5;
+
+const result = add(number1, number2);
+console.log(`the result is: ${result}`);
