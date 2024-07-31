90af5fcfe46f4d8028222ab9ea5d63cedd80d0a5
Eric - Wall Sound <emoolenbeek@gmail.com>
2024-07-31 08:33:51 -0400

    add comments

--- typescript/classes-and-interfaces/app.ts
+++ typescript/classes-and-interfaces/app.ts
@@ -1,11 +1,15 @@
+// classes
+
 class Department {
   name: string;
 
+  //constructor method
   constructor(n: string) {
     this.name = n;
   }
 }
 
+//create instance of class
 const shipping = new Department("Shipping");
 
 console.log(shipping);
