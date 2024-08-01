df5b155d8b858fc03c39ee1e765266e400752fb6
Eric - Wall Sound <emoolenbeek@gmail.com>
2024-07-31 21:14:57 -0400

    add example of method and this keyword

--- typescript/classes-and-interfaces/app.ts
+++ typescript/classes-and-interfaces/app.ts
@@ -7,9 +7,16 @@ class Department {
   constructor(n: string) {
     this.name = n;
   }
+
+  // method
+  describe (this: Department) {
+    console.log('Department: ' + this.name);
+  }
 }
 
 //create instance of class
 const shipping = new Department("Shipping");
 
 console.log(shipping);
+
+shipping.describe();
