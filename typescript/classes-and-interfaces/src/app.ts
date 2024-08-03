18a84758176148972d23d28c05f28a5d0c7c08ee
Eric - Wall Sound <emoolenbeek@gmail.com>
2024-08-03 15:48:27 -0400

    create src folder

---
+++ typescript/classes-and-interfaces/src/app.ts
@@ -0,0 +1,22 @@
+// classes
+
+class Department {
+  name: string;
+
+  //constructor method
+  constructor(n: string) {
+    this.name = n;
+  }
+
+  // method
+  describe(this: Department) {
+    console.log("Department: " + this.name);
+  }
+}
+
+//create instance of class
+const shipping = new Department("Shipping");
+
+console.log(shipping);
+
+shipping.describe();
