2ecc167e680725b87514cdf2827f5b90e77ca1f5
Eric - Wall Sound <emoolenbeek@gmail.com>
2024-08-04 13:15:18 -0400

    add examples of private access

---
+++ typescript/classes-and-interfaces/src/app.ts
@@ -0,0 +1,29 @@
+class Department {
+  name: string;
+  private employees: string[] = [];
+
+  constructor(n: string) {
+    this.name = n;
+  }
+
+  describe(this: Department) {
+    console.log("Department: " + this.name);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
+}
+
+const shipping = new Department("Shipping");
+
+shipping.addEmployee("Bob");
+shipping.addEmployee("Joe");
+
+shipping.describe();
+shipping.printEmployeeInformation();
